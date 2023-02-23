1fbfeb5eecb5ad586859a9d26405d894f6e0ed4f
ARC2023 <arcautomation2023@gmail.com>
2023-02-23 09:37:17 +0530

    First COde Commit

---
+++ src/com/qa/git/demo/NewClass.java
@@ -0,0 +1,9 @@
+package com.qa.git.demo;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/qa/github/demo/GitHubDemo.java
@@ -0,0 +1,9 @@
+package com.qa.github.demo;
+
+public class GitHubDemo {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub Demo");
+	}
+
+}
